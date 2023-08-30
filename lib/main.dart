2d4a0dc0640a84e3ed88f8b8354745f20389b297
mohammed3helles <hlsmhmd1@gmail.com>
2023-08-30 09:58:08 +0300

    note app

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class NoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text('data'),
+        title: const Text('data is required'),
       ),
     );
   }
