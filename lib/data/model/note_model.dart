849cdb5cddd9ec0504265d53eebd8b588059b68b
mohammed3helles <hlsmhmd1@gmail.com>
2023-08-31 04:29:32 +0300

    create type adapter for note

--- lib/data/model/note_model.dart
+++ lib/data/model/note_model.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
 
-class Note {
-  final int? id;
+part 'note_model.g.dart';
+// flutter pub run build_runner build this code to use build runner
+@HiveType(typeId: 0)
+class Note extends HiveObject {
+  @HiveField(0)
   final String? title;
+  @HiveField(1)
   final String? description;
+  @HiveField(2)
   final String? date;
+  @HiveField(3)
   final Color? color;
 
   Note({
-    required this.id,
     required this.title,
     required this.description,
     required this.date,
